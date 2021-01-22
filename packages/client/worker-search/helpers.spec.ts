04dd94313c52b53ee880b5290607e8795e5e9191
Tiger Oakes <contact@tigeroakes.com>
2021-01-21 19:50:13 -0800

    Add jest

---
+++ packages/client/worker-search/helpers.spec.ts
@@ -0,0 +1,12 @@
+import { applyOffset } from './helpers';
+
+test('applyOffset', async () => {
+  expect(applyOffset('Google abc', 3)).toBe('Goo abc');
+  expect(applyOffset('Google abc', 8)).toBe('Google a');
+
+  expect(applyOffset('Google abc', 10)).toBe('Google abc');
+  expect(applyOffset('Google abc', 6)).toBe('Google abc');
+
+  expect(applyOffset('Google abc', 0)).toBe(' abc');
+  expect(applyOffset('Google abc', 7)).toBe('Google ');
+});
