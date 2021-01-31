8e177a343cc884cd28d5f53cc3c003dc6e8f3859
Tiger Oakes <contact@tigeroakes.com>
2021-01-30 23:58:53 -0800

    Npx all

--- netlify.sh
+++ netlify.sh
@@ -1,4 +1,4 @@
 sed -i "s|{IPSTACK_KEY}|${IPSTACK_KEY}|g" netlify.toml
 npx pnpm install -r --store=node_modules/.pnpm-store || echo skiping pnpm install
-pnpm run build --filter @hawaii-bus-plus/client
-pnpm run build --filter @hawaii-bus-plus/functions
+npx pnpm run build --filter @hawaii-bus-plus/client
+npx pnpm run build --filter @hawaii-bus-plus/functions
