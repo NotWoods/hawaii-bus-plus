748a1713227ac9a247ff208ecca97a82743de13b
Tiger Oakes <contact@tigeroakes.com>
2021-01-31 23:01:02 -0800

    npm

--- netlify.sh
+++ netlify.sh
@@ -1,6 +1,6 @@
 sed -i "s|{IPSTACK_KEY}|${IPSTACK_KEY}|g" netlify.toml
 npx pnpm install -r --frozen-lockfile --store=node_modules/.pnpm-store || echo skiping pnpm install
-npx i -g typescript
+npm i -g typescript
 npx pnpm run build --filter @hawaii-bus-plus/client
 npx pnpm run build --filter @hawaii-bus-plus/functions
 npx pnpm run api:dist --filter @hawaii-bus-plus/api-gen
