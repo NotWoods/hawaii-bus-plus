9187bb539303118b3d87f811e9aa54b7ab8b3a2d
Tiger Oakes <contact@tigeroakes.com>
2021-01-31 23:14:38 -0800

    Add content type to API

--- packages/functions/src/api/api.ts
+++ packages/functions/src/api/api.ts
@@ -12,8 +12,16 @@ export async function handler(
     const path = require.resolve(event.path.replace('/api/v1/', './'));
     console.log(event.path, path);
     const file = await readFileAsync(path, 'utf8');
-    return { statusCode: 200, body: file };
+    return {
+      statusCode: 200,
+      body: file,
+      headers: { 'Content-Type': 'application/json' },
+    };
   } else {
-    return { statusCode: 401, body: JSON.stringify({ error: 'Unauthorized' }) };
+    return {
+      statusCode: 401,
+      body: JSON.stringify({ error: 'Unauthorized' }),
+      headers: { 'Content-Type': 'application/json' },
+    };
   }
 }
