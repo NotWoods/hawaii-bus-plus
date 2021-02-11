6eb7d8826f548020dd2bdddaba305e85a5e06392
Tiger Oakes <contact@tigeroakes.com>
2021-02-10 22:24:47 -0800

    Add exit condition

--- scripts/app.sh
+++ scripts/app.sh
@@ -1,5 +1,5 @@
 sed -i "s|{IPSTACK_KEY}|${IPSTACK_KEY}|g" netlify.toml
 npx pnpm recursive install --frozen-lockfile --store=node_modules/.pnpm-store || echo skiping pnpm install
-npx pnpm run build --filter @hawaii-bus-plus/client
-npx pnpm run build --filter @hawaii-bus-plus/functions
-npx pnpm run api:dist --filter @hawaii-bus-plus/api-gen
+npx pnpm run build --filter @hawaii-bus-plus/client || exit 1
+npx pnpm run build --filter @hawaii-bus-plus/functions || exit 1
+npx pnpm run api:dist --filter @hawaii-bus-plus/api-gen || exit 1
