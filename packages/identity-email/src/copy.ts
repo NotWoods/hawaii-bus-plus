d3d521653d0c272b4456896e207282e4018f96a6
Tiger Oakes <contact@tigeroakes.com>
2021-02-19 21:29:12 -0800

    Add copy script

---
+++ packages/identity-email/src/copy.ts
@@ -0,0 +1,40 @@
+import { mkdir, readdir, readFile, writeFile } from 'fs/promises';
+
+const templatesFolder = new URL('../templates/', import.meta.url);
+const distFolder = new URL('../../../dist/', templatesFolder);
+
+function fileAlreadyExists(err: unknown): err is Error {
+  return (err as { code?: string }).code === 'EEXIST';
+}
+
+async function mkdirIfNotExists(folder: string | URL) {
+  try {
+    await mkdir(folder);
+  } catch (err: unknown) {
+    if (fileAlreadyExists(err)) {
+      // Folder already exists
+    } else {
+      throw err;
+    }
+  }
+}
+
+async function makeOutDir() {
+  await mkdirIfNotExists(distFolder);
+  await mkdirIfNotExists(new URL(`./auth/`, distFolder));
+  await mkdirIfNotExists(new URL(`./auth/email/`, distFolder));
+}
+
+async function copyFile(file: string) {
+  const path = new URL(`./${file}`, templatesFolder);
+  const body = await readFile(path);
+
+  const outPath = new URL(`./auth/email/${file}`, distFolder);
+  await writeFile(outPath, body, 'utf8');
+}
+
+async function main() {
+  const [files] = await Promise.all([readdir(templatesFolder), makeOutDir()]);
+  await Promise.all(files.map(copyFile));
+}
+main().catch((err) => console.error(err));
