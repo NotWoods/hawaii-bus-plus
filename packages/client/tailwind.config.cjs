a0872debfa72ad4c8c48a7db0320ac67c906bfb2
Tiger Oakes <contact@tigeroakes.com>
2021-03-15 10:30:50 -0700

    Clean up tailwind colors

--- packages/client/tailwind.config.cjs
+++ packages/client/tailwind.config.cjs
@@ -13,6 +13,44 @@ module.exports = {
     fontFamily: {
       display: ['Red Rose', 'Copperplate Gothic', 'Copperplate', 'serif'],
     },
+    colors: {
+      black: colors.black,
+      white: colors.white,
+      route: 'var(--route-color)',
+      gray: {
+        ...colors.trueGray,
+        750: '#333',
+      },
+      blue: {
+        50: 'hsl(212, 20%, 90%)',
+        100: 'hsl(212, 10%, 72%)',
+        200: 'hsl(212, 10%, 64%)',
+        300: 'hsl(212, 10%, 53%)',
+        400: 'hsl(212, 10%, 42%)',
+        500: 'hsl(212, 10%, 32%)',
+        600: 'hsl(212, 10%, 28%)',
+        700: 'hsl(212, 10%, 22%)',
+        800: 'hsl(212, 10%, 18%)',
+        900: 'hsl(212, 10%, 10%)',
+      },
+      red: {
+        DEFAULT: '#C67168',
+      },
+      yellow: {
+        DEFAULT: '#E2C049',
+      },
+      brown: {
+        DEFAULT: '#918381',
+      },
+      cyan: {
+        DEFAULT: '#8BB9BD',
+      },
+      ocean: {
+        light: '#778a91',
+        DEFAULT: '#335468',
+        dark: '#2d4859',
+      },
+    },
     extend: {
       inset: {
         '1/4-screen': '25vh',
@@ -20,42 +58,6 @@ module.exports = {
       },
       minHeight: {
         '1/4-screen': '25vh',
-      },
-      colors: {
-        route: 'var(--route-color)',
-        gray: {
-          ...colors.trueGray,
-          750: '#333',
-        },
-        blue: {
-          50: 'hsl(212, 20%, 90%)',
-          100: 'hsl(212, 10%, 72%)',
-          200: 'hsl(212, 10%, 64%)',
-          300: 'hsl(212, 10%, 53%)',
-          400: 'hsl(212, 10%, 42%)',
-          500: 'hsl(212, 10%, 32%)',
-          600: 'hsl(212, 10%, 28%)',
-          700: 'hsl(212, 10%, 22%)',
-          800: 'hsl(212, 10%, 18%)',
-          900: 'hsl(212, 10%, 10%)',
-        },
-        red: {
-          DEFAULT: '#C67168',
-        },
-        yellow: {
-          DEFAULT: '#E2C049',
-        },
-        brown: {
-          DEFAULT: '#918381',
-        },
-        cyan: {
-          DEFAULT: '#8BB9BD',
-        },
-        ocean: {
-          light: '#778a91',
-          DEFAULT: '#335468',
-          dark: '#2d4859',
-        },
       },
       fill: (theme) => ({
         'blue-500': theme('colors.blue.500'),
