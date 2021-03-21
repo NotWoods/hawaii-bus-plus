093c402631a73d1b7e54b0a9b9f286a4ae28c636
Tiger Oakes <contact@tigeroakes.com>
2021-03-21 14:39:03 -0700

    Add pwa stuff

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -1,5 +1,6 @@
 import { emptyPackage, prefreshPlus } from '@hawaii-bus-plus/vite-plugins';
 import { AliasOptions, defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
 
 const productionMode = process.env.NETLIFY_CONTEXT === 'production';
 const alias: AliasOptions = {
@@ -13,7 +14,60 @@ if (productionMode) {
 }
 
 export default defineConfig({
-  plugins: [emptyPackage('preact/debug'), prefreshPlus()],
+  plugins: [
+    emptyPackage('preact/debug'),
+    prefreshPlus(),
+    VitePWA({
+      mode: productionMode ? 'production' : 'development',
+      minify: productionMode,
+      workbox: {
+        navigateFallback: '/index.html',
+        navigateFallbackAllowlist: [
+          new RegExp('/routes/'),
+          new RegExp('/directions'),
+        ],
+      },
+      manifest: {
+        short_name: 'Hawaii Bus+',
+        name: 'Hawaii Bus Plus',
+        start_url: '/',
+        background_color: '#f9fafb',
+        theme_color: '#32383e',
+        icons: [
+          {
+            src: '/icon/transparent.png',
+            type: 'image/png',
+            sizes: '512x512',
+            purpose: 'any',
+          },
+          {
+            src: '/icon/maskable.png',
+            type: 'image/png',
+            sizes: '512x512',
+            purpose: 'maskable',
+          },
+          {
+            src: '/icon/small.png',
+            type: 'image/png',
+            sizes: '48x48',
+            purpose: 'any',
+          },
+          {
+            src: '/icon/monochrome.png',
+            type: 'image/png',
+            sizes: '48x48',
+            purpose: 'monochrome',
+          },
+          {
+            src: '/icon/monochrome.svg',
+            type: 'image/svg+xml',
+            sizes: 'any',
+            purpose: 'monochrome',
+          },
+        ],
+      },
+    }),
+  ],
   resolve: { alias },
   optimizeDeps: {
     include: ['preact', 'preact/debug', 'preact/hooks'],
