6a7c7bb85ad317957efc5ca57fb8b88a1fb96190
Tiger Oakes <contact@tigeroakes.com>
2021-11-12 09:11:42 -0800

    FIx react slip in

--- packages/tailwind-theme/src/index.ts
+++ packages/tailwind-theme/src/index.ts
@@ -1,9 +1,2 @@
-import { readFile } from 'fs/promises';
-import { resolve } from 'path';
-
 export { theme } from './theme.js';
-
-export function headHtml() {
-  const filePath = resolve(__dirname, '../head.html');
-  return readFile(filePath, 'utf8');
-}
+export { headHtml } from './paths.js';
