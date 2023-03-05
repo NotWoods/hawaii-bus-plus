faf595c0fd2b550be76fc4419b663d51565f0272
Tiger Oakes <contact@tigeroakes.com>
2023-03-05 14:57:48 -0800

    Remove gotrue

--- scripts/app.sh
+++ scripts/app.sh
@@ -1,3 +1,2 @@
 npx pnpm recursive install --frozen-lockfile --store=node_modules/.pnpm-store || echo skiping pnpm install
 npx pnpm run build || exit 1
-npx pnpm run build --filter @hawaii-bus-plus/identity-email || exit 1
