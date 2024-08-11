ecbae22441828260743a264e97df262489148da4
Tiger Oakes <contact@tigeroakes.com>
2024-08-10 17:22:20 -0700

    Use prod env

--- scripts/app.sh
+++ scripts/app.sh
@@ -1,3 +1,3 @@
 node --version
-pnpm recursive install --frozen-lockfile --store=node_modules/.pnpm-store || echo skiping pnpm install
+pnpm recursive install --prod=false --frozen-lockfile --store=node_modules/.pnpm-store || echo skiping pnpm install
 pnpm run build || exit 1
