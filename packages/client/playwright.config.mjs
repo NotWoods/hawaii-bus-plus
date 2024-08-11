0af2e8d81133535fdfb4921191a07869134ec000
Tiger Oakes <contact@tigeroakes.com>
2024-08-11 15:04:31 -0700

    Add playwright tests

---
+++ packages/client/playwright.config.mjs
@@ -0,0 +1,62 @@
+// @ts-check
+import { defineConfig, devices } from '@playwright/test';
+
+const webServer = true;
+
+/**
+ * See https://playwright.dev/docs/test-configuration.
+ */
+export default defineConfig({
+  testDir: './e2e',
+  /* Run tests in files in parallel */
+  fullyParallel: true,
+  /* Fail the build on CI if you accidentally left test.only in the source code. */
+  forbidOnly: !!process.env.CI,
+  /* Retry on CI only */
+  retries: process.env.CI ? 2 : 0,
+  workers: process.env.CI ? 1 : undefined,
+  use: {
+    baseURL: webServer
+      ? 'http://localhost:4321'
+      : 'https://app.hawaiibusplus.com/',
+    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+    trace: 'on-first-retry',
+  },
+
+  /* Configure projects for major browsers */
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
+
+    {
+      name: 'webkit',
+      use: { ...devices['Desktop Safari'] },
+    },
+
+    /* Test against mobile viewports. */
+    {
+      name: 'Mobile Chrome',
+      use: { ...devices['Pixel 5'] },
+    },
+    {
+      name: 'Mobile Safari',
+      use: { ...devices['iPhone 12'] },
+    },
+  ],
+
+  /* Run your local dev server before starting the tests */
+  webServer: webServer
+    ? {
+        command: 'pnpm run preview',
+        port: 4321,
+        reuseExistingServer: !process.env.CI,
+      }
+    : undefined,
+});
