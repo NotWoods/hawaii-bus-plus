9d3ac0c6d90738fa75759bffab9e0498de904b06
Tiger Oakes <contact@tigeroakes.com>
2024-08-11 22:54:20 -0700

    Improve light mode contrast

--- packages/client/page/sheet/BaseSheet.tsx
+++ packages/client/page/sheet/BaseSheet.tsx
@@ -17,7 +17,7 @@ export function BaseSheet(props: Props) {
     <div class={clsx('relative z-10 md:ml-80', { 'top-[75vh]': !loadError })}>
       <article
         class={clsx(
-          'animate-enter motion-reduce:animate-duration-0 shadow-lg bg-gradient-to-br from-gray-50 to-gray-100 to:gray-100 dark:from-gray-750 dark:to-gray-800 text-black dark:text-white lg:mx-4',
+          'animate-enter motion-reduce:animate-duration-0 shadow-lg bg-gradient-to-br from-gray-100 to-primary-50 dark:from-gray-750 dark:to-gray-800 text-black dark:text-white lg:mx-4',
           { 'min-h-[25vh]': !loadError },
           props.loaded ? 'animate-run' : 'animate-pause',
         )}
